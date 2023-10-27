6bdfa70d271e425a0e65968e098a8d96c7b97b16
Aqeel <aqeelten@gmail.com>
2023-10-27 22:52:46 +0530

    update

---
+++ src/app/tests/page.jsx
@@ -0,0 +1,14 @@
+import { Configuration, OpenAIApi } from "openai";
+
+export default function Tests() {
+    return (
+      <main>
+       <h1>test</h1>
+       <textarea>
+
+       </textarea>
+       <button className="mt-100">Button</button>
+      </main>
+    )
+  }
+  
