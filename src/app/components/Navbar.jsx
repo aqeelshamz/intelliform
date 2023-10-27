11d2afe7bfd8f408482f719282ae96c75aa16912
Aqeel <aqeelten@gmail.com>
2023-10-28 01:07:22 +0530

    navbar - connect wallet button

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,46 @@
 import { FiUser } from "react-icons/fi";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  ConnectButton
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  zora,
+} from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
 function Navbar() {
+  const { chains, publicClient } = configureChains(
+    [polygon],
+    [
+      alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+      publicProvider()
+    ]
+  );
+  const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    projectId: 'a938bee08643184fb038b8deec30437f',
+    chains
+  });
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient
+  })
+
   return (
-    <>
-      <div className="navbar bg-base-100">
-        {/* <div className="flex-none">
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <div className="navbar bg-base-100">
+          {/* <div className="flex-none">
           <button className="btn btn-square btn-ghost">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -20,16 +57,15 @@ function Navbar() {
             </svg>
           </button>
         </div> */}
-        <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">IntelliForm</a>
+          <div className="flex-1">
+            <a className="btn btn-ghost normal-case text-xl">IntelliForm</a>
+          </div>
+          <div className="flex-none">
+            <ConnectButton />
+          </div>
         </div>
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <FiUser />
-          </button>
-        </div>
-      </div>
-    </>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
