70fe543b034cad9ab1f663e083df294893443c82
Abdul Majid <majid616365@gmail.com>
2023-10-28 01:11:42 +0530

    f: update contract

--- contract/pay.sol
+++ contract/pay.sol
@@ -2,18 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract FormContract {
-    address public formCreator; // The address of the form creator
-    uint256 public minimumPayment;
-
-    constructor(address _formCreator, uint256 _minimumPayment) {
-        formCreator = _formCreator;
-        minimumPayment = _minimumPayment;
-    }
-
-    // Function to allow users to register and pay
-    function register() external payable {
-        require(msg.value >= minimumPayment, "Payment must be at least the minimum required amount");
-        payable(formCreator).transfer(msg.value);
+    function register(address payable recipient) external payable {
+        require(recipient != address(0), "Recipient address cannot be zero");
+        recipient.transfer(msg.value);
     }
 
     // Function to check the contract's balance
