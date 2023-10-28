6977d32cb499ea42b1dce40ddfe5372cad78ed8e
sai-na <saipkmm100@gmail.com>
2023-10-28 14:52:40 +0530

    Update page.jsx

--- src/app/editor/[formId]/page.jsx
+++ src/app/editor/[formId]/page.jsx
@@ -22,6 +22,7 @@ export default function Home({ params: { formId } }) {
   // };
 
   const [form, setForm] = useState();
+  const [tab, setTab] = useState(true);
 
   const [loadingFormData, setLoadingFormData] = useState(true);
 
@@ -68,93 +69,98 @@ export default function Home({ params: { formId } }) {
           Save
         </button>
       </div>
-      <main className="container mx-auto relative mt-6 ">
-        {loadingFormData ? (
-          <div>
-            <span className="loading loading-spinner loading-lg"></span>
-          </div>
-        ) : (
-          <div className="border-black w-full border-2 h-auto rounded-xl p-3 pl-8 mb-20 pb-20">
-            <div className="row0 flex justify-end">
-              <button className="btn  text-red-500 hover:bg-red-500 hover:border-white border-red-500 btn-outline">
-                <FiTrash2 className="h-6 w-6 " />
-              </button>
+
+      {tab && (
+        <main className="container mx-auto relative mt-6 ">
+          {loadingFormData ? (
+            <div>
+              <span className="loading loading-spinner loading-lg"></span>
             </div>
-            <div className="row1 title">
-              <div className="flex items-center gap-3">
-                <span className="text-3xl font-bold ">{form?.title}</span>{" "}
-                <FiEdit />
+          ) : (
+            <div className="border-black w-full border-2 h-auto rounded-xl p-3 pl-8 mb-20 pb-20">
+              <div className="row0 flex justify-end">
+                <button className="btn  text-red-500 hover:bg-red-500 hover:border-white border-red-500 btn-outline">
+                  <FiTrash2 className="h-6 w-6 " />
+                </button>
+              </div>
+              <div className="row1 title">
+                <div className="flex items-center gap-3">
+                  <span className="text-3xl font-bold ">{form?.title}</span>{" "}
+                  <FiEdit />
+                </div>
+
+                <div className="flex items-center gap-3 mt-3">
+                  <span className="text-xl">{form?.description}</span>{" "}
+                  <FiEdit />
+                </div>
               </div>
 
-              <div className="flex items-center gap-3 mt-3">
-                <span className="text-xl">{form?.description}</span> <FiEdit />
-              </div>
-            </div>
-
-            <div className="inputs">
-              {form?.fields?.map((field, index) => {
-                return (
-                  <>
-                    <div className="inputrow">
-                      <div className="flex items-center gap-3 mt-5">
-                        <label className="text-xl font-semibold">
-                          {field?.title}
-                        </label>
-                        <FiEdit />
-                      </div>
-                      <div className="flex items-center gap-3 mt-3">
-                        {field?.type === "multiplechoice" ? (
-                          <select className="w-full max-w-4xl select select-bordered">
-                            {field?.choices?.map((option, i) => {
-                              return <option key={i}>{option}</option>;
-                            })}
-                          </select>
-                        ) : field?.type === "longtext" ? (
-                          <textarea
-                            className="w-full max-w-4xl textarea textarea-bordered"
-                            placeholder={field?.title}
-                          ></textarea>
-                        ) : (
-                          <input
-                            className="w-full max-w-4xl input input-bordered"
-                            type={field?.type}
-                            placeholder={field?.title}
-                          />
-                        )}
-                        <button className="btn btn-sm h-[45px] w-[45px] btn-square btn-outline">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              d="M6 18L18 6M6 6l12 12"
+              <div className="inputs">
+                {form?.fields?.map((field, index) => {
+                  return (
+                    <>
+                      <div className="inputrow">
+                        <div className="flex items-center gap-3 mt-5">
+                          <label className="text-xl font-semibold">
+                            {field?.title}
+                          </label>
+                          <FiEdit />
+                        </div>
+                        <div className="flex items-center gap-3 mt-3">
+                          {field?.type === "multiplechoice" ? (
+                            <select className="w-full max-w-4xl select select-bordered">
+                              {field?.choices?.map((option, i) => {
+                                return <option key={i}>{option}</option>;
+                              })}
+                            </select>
+                          ) : field?.type === "longtext" ? (
+                            <textarea
+                              className="w-full max-w-4xl textarea textarea-bordered"
+                              placeholder={field?.title}
+                            ></textarea>
+                          ) : (
+                            <input
+                              className="w-full max-w-4xl input input-bordered"
+                              type={field?.type}
+                              placeholder={field?.title}
                             />
-                          </svg>
-                        </button>
+                          )}
+                          <button className="btn btn-sm h-[45px] w-[45px] btn-square btn-outline">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-6 w-6"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M6 18L18 6M6 6l12 12"
+                              />
+                            </svg>
+                          </button>
+                        </div>
                       </div>
-                    </div>
-                  </>
-                );
-              })}
+                    </>
+                  );
+                })}
 
-              <button
-                className="btn mt-10 mb-5 btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-black hover:bg-gray-700  text-white w-96 lg:w-[896px]"
-                onClick={() =>
-                  document.getElementById("my_modal_1").showModal()
-                }
-              >
-                Add Content
-              </button>
+                <button
+                  className="btn mt-10 mb-5 btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-black hover:bg-gray-700  text-white w-96 lg:w-[896px]"
+                  onClick={() =>
+                    document.getElementById("my_modal_1").showModal()
+                  }
+                >
+                  Add Content
+                </button>
+              </div>
             </div>
-          </div>
-        )}
-      </main>
+          )}
+        </main>
+      )}
+
       {/* Modals */}
       {/* modal 1 */}
       <dialog id="my_modal_1" className="modal">
